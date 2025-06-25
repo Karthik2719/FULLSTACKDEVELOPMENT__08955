0d3baade87209930fe75058489cfcd5431eadfaa
Eranagari Karthik <eranagarikarthik@gmail.com>
2025-06-25 15:14:22 +0530

    javascripts concepts

---
+++ Dom.js
@@ -0,0 +1,13 @@
+document.title = "Dom in JavaScript";
+ document.getElementById("header").innerHTML="Hello,World!";
+
+ document.getElementById("para").style.color = "blue";
+ document.getElementById("para").style.fontSize = "30px";
+
+
+document.getElementsByClassName("ds")[0].innerHTML="KARTHIK";
+document.getElementsByClassName("ds")[0].style.color="yellow";
+document.getElementsByClassName("ds")[0].style.fontSize="50px";
+document.getElementsByTagName("h1")[1].innerHTML="MALLA REDDY COLLEGE";
+document.getElementsByTagName("h1")[1].style.color="red";
+document.body.style.backgroundColor ="lightblue";
---
+++ code.js
@@ -0,0 +1,50 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "professorBot";
+let botLocation = "Codeville";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is primarily used for web development.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can run both on the client and the server.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
+
+
+
+
+
+
+
+
+
+//Build a Sentence Maker
+let adjective = "fuzzy";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "sparkly";
+let noun2 = "marshmallows";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "slimy";
+noun = "goblin";
+verb = "scared";
+place = "swamp";
+adjective2 = "greasy";
+noun2 = "cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
